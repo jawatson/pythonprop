0aaab2833213a1de24e0e999f8bea87f740ba824
James Watson <jwatson@neomailbox.ch>
2015-11-30 21:01:55 +0300

    Builds an index of the start of each data set.

--- python3Prop/src/rec533Out.py
+++ python3Prop/src/rec533Out.py
@@ -22,8 +22,6 @@
 
 import csv
 
-from mpl_toolkits.basemap import Basemap
-import matplotlib.pyplot as plt
 import numpy as np
 
 from voaAreaRect import VOAAreaRect
@@ -46,9 +44,43 @@ class REC533Out:
     def __init__(self, filename):
         self.filename = filename
         # todo each of the following parameters should be read from the outfile
-        self.plot_rect = VOAAreaRect() # default is global
+        self.plot_rect = VOAAreaRect()
         self.lat_step = 5.0
         self.lon_step = 5.0
+        #self.parse_input_params()
+        self.datasets = self.build_dataset_list()
+        print ("Found ",len(self.datasets), " datasets")
+
+        with open(filename, 'r') as f:
+            for month, hour, freq, offset in self.datasets:
+                f.seek(offset)
+                print(f.readline().strip())
+
+
+    def build_dataset_list(self):
+        datasets = []
+        month = ''
+        hour = ''
+        freq = ''
+        idx = 0
+        calculated_parameters_section = False
+        with open(self.filename) as f:
+            for line in f:
+                if "**** Calculated Parameters ****" in line:
+                    calculated_parameters_section = True
+                elif calculated_parameters_section:
+                    params = line.split(',')
+                    if len(params) > 3:
+                        if (month != params[0]) or (hour != params[1]) or (freq != params[2]):
+                            month = params[0]
+                            hour = params[1]
+                            freq =params[2]
+                            datasets.append((month, hour, freq, idx))
+                            print (month, hour, freq, idx)
+                idx += len(line)+1
+        return datasets
+
+
 
     def __iter__(self):
         pass
@@ -58,9 +90,7 @@ class REC533Out:
 
     def get_plot_data(self, hour, freq):
         num_pts_lat = ((self.plot_rect.get_ne_lat() - self.plot_rect.get_sw_lat()) / self.lat_step) + 1
-        print ("num_pts_lat = ", num_pts_lat)
         num_pts_lon = ((self.plot_rect.get_ne_lon() - self.plot_rect.get_sw_lon()) / self.lon_step) + 1
-        print ("num_pts_lon = ", num_pts_lon)
         points = np.zeros([num_pts_lat, num_pts_lon], float)
 
         lons = np.arange(self.plot_rect.get_sw_lon(),
@@ -78,48 +108,8 @@ class REC533Out:
                         #print (row)
                         #todo remove the hardcodes values of 3, 4 and 8 below
                         lat_grid_pos = (int(float(row[3])-self.plot_rect.get_sw_lat()) / self.lat_step)
-                        #print (float(row[3]))
-                        #print (self.plot_rect.get_sw_lat())
-                        #print ("lat_grid_pos =", lat_grid_pos)
                         lon_grid_pos = (int(float(row[4])-self.plot_rect.get_sw_lon()) / self.lon_step)
-                        #print ("lon_grid_pos =", lon_grid_pos)
                         points[lat_grid_pos][lon_grid_pos] = float(row[8])
-                        #print (lat_grid_pos, lon_grid_pos, float(row[8]))
-
         finally:
             f.close()
         return (points,lons,lats,num_pts_lon,num_pts_lat)
-
-'''
-The following function is for testing purposes only.
-'''
-if __name__ == "__main__":
-    r533 = REC533Out("area.out")
-    points,lons,lats,num_pts_lon,num_pts_lat = r533.get_plot_data('01', '3.52')
-
-    plt.figure(figsize=(12,6))
-    m = Basemap(projection='cyl', resolution='l')
-    m.drawcoastlines(color='black')
-    m.drawcountries(color='grey')
-    m.drawmapboundary(color='black', linewidth=1.0)
-    # draw lat/lon grid lines every 30 degrees.
-    m.drawmeridians(np.arange(0,360,30))
-    m.drawparallels(np.arange(-90,90,30))
-
-    print ("points: ", points.shape)
-    print ("lons: ", len(lons))
-    print ("lats: ", len(lats))
-    print ("num_lon: ", num_pts_lon)
-    print ("num_lat: ", num_pts_lat)
-    X,Y = np.meshgrid(lons, lats)
-    print ("X: ", X.shape)
-
-    print ("Y: ", Y.shape)
-
-    im1 = m.pcolormesh(X, Y, points, shading='gouraud', cmap=plt.cm.jet, latlon=True)
-    cb = m.colorbar(im1,"bottom", size="5%", pad="2%")
-    #plt.title('Test ITU Image')
-
-    plt.savefig('foo.png', dpi=400, bbox_inches='tight')
-
-    plt.show()
