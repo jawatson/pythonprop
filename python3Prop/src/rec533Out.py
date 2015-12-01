26d51ae6c8262587bef2c9d8dec47777629a93a6
James Watson <jwatson@neomailbox.ch>
2015-12-01 16:33:08 +0300

    Global config args are now read from the rec533 data file.

--- python3Prop/src/rec533Out.py
+++ python3Prop/src/rec533Out.py
@@ -21,6 +21,8 @@
 #
 
 import csv
+import datetime
+import re
 
 import numpy as np
 
@@ -43,22 +45,35 @@ class REC533Out:
 
     def __init__(self, filename):
         self.filename = filename
-        # todo each of the following parameters should be read from the outfile
         self.plot_rect = VOAAreaRect()
-        self.lat_step = 5.0
-        self.lon_step = 5.0
-        #self.parse_input_params()
+        self.lat_step_size, self.lon_step_size = self.parse_global_params()
         self.datasets = self.build_dataset_list()
         print ("Found ",len(self.datasets), " datasets")
+        self.itr_ctr = -1
 
-        with open(filename, 'r') as f:
-            for month, hour, freq, offset in self.datasets:
-                f.seek(offset)
-                print(f.readline().strip())
+
+    def parse_global_params(self):
+        lat_inc_pattern = re.compile("^\s*Latitude increment\s*= ([\d.]+) \(deg\)")
+        lon_inc_pattern = re.compile("^\s*Longitude increment\s*= ([\d.]+) \(deg\)")
+
+        with open(self.filename) as f:
+            for line in f:
+                m = lat_inc_pattern.match(line)
+                if m:
+                    lat_inc = float(m.group(1))
+                m = lon_inc_pattern.match(line)
+                if m:
+                    lon_inc = float(m.group(1))
+
+                if '*** Calculated Parameters ***' in line:
+                    break
+        return (lat_inc, lon_inc)
 
 
     def build_dataset_list(self):
         datasets = []
+        #todo remove hardcoded year
+        year = '2015'
         month = ''
         hour = ''
         freq = ''
@@ -75,41 +90,53 @@ class REC533Out:
                             month = params[0]
                             hour = params[1]
                             freq =params[2]
-                            datasets.append((month, hour, freq, idx))
-                            print (month, hour, freq, idx)
+                            h = int(hour) if (int(hour) < 24) else int(hour) % 24
+                            plot_dt = datetime.datetime(int(year), int(month), 15, hour=h)
+                            datasets.append((plot_dt, freq.strip(), idx))
+                            #print (month, hour, freq, idx)
                 idx += len(line)+1
         return datasets
 
 
-
     def __iter__(self):
-        pass
+        return self
+
 
     def __next__(self):
-        pass
+        self.itr_ctr += 1
+        if self.itr_ctr < (len(self.datasets)):
+            return self.get_plot_data(self.datasets[self.itr_ctr])
+        else:
+            raise StopIteration
 
-    def get_plot_data(self, hour, freq):
-        num_pts_lat = ((self.plot_rect.get_ne_lat() - self.plot_rect.get_sw_lat()) / self.lat_step) + 1
-        num_pts_lon = ((self.plot_rect.get_ne_lon() - self.plot_rect.get_sw_lon()) / self.lon_step) + 1
+
+    def get_plot_data(self, dataset_params):
+        plot_dt, freq, idx = dataset_params
+        num_pts_lat = ((self.plot_rect.get_ne_lat() - self.plot_rect.get_sw_lat()) / self.lat_step_size) + 1
+        num_pts_lon = ((self.plot_rect.get_ne_lon() - self.plot_rect.get_sw_lon()) / self.lon_step_size) + 1
         points = np.zeros([num_pts_lat, num_pts_lon], float)
 
         lons = np.arange(self.plot_rect.get_sw_lon(),
                     self.plot_rect.get_ne_lon()+1,
-                    self.lon_step)
+                    self.lon_step_size)
         lats = np.arange(self.plot_rect.get_sw_lat(),
                     self.plot_rect.get_ne_lat()+1,
-                    self.lat_step)
+                    self.lat_step_size)
         f = open(self.filename, 'rt')
+        #f.seek(idx)
+        freq=freq.strip()
+        formatted_hour_str = '{0:02d}'.format(plot_dt.hour)
         try:
             reader = csv.reader(f)
             for row in reader:
                 if len(row) > 3:
-                    if row[1].strip()==hour and row[2].strip()==freq:
+                    #print("looking in +", row[1].strip(), "+ for +", formatted_hour_str, ": looking in +", row[2].strip(), "+ for +", freq,"+")
+                    if row[1].strip()==formatted_hour_str and row[2].strip()==freq:
                         #print (row)
                         #todo remove the hardcodes values of 3, 4 and 8 below
-                        lat_grid_pos = (int(float(row[3])-self.plot_rect.get_sw_lat()) / self.lat_step)
-                        lon_grid_pos = (int(float(row[4])-self.plot_rect.get_sw_lon()) / self.lon_step)
+                        lat_grid_pos = (int(float(row[3])-self.plot_rect.get_sw_lat()) / self.lat_step_size)
+                        lon_grid_pos = (int(float(row[4])-self.plot_rect.get_sw_lon()) / self.lon_step_size)
                         points[lat_grid_pos][lon_grid_pos] = float(row[8])
         finally:
             f.close()
-        return (points,lons,lats,num_pts_lon,num_pts_lat)
+        return (points, lons, lats, num_pts_lon, num_pts_lat, dataset_params)
