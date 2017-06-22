4fca8189ed6cbf756fabfe2bb5a2df3fbbefe450
James Watson <jwatson@neomailbox.ch>
2017-06-22 21:02:11 +0300

    Casts a float to and int to clear issue #4 (Slice index must be integers error.)

--- src/pythonprop/voaP2PPlot.py
+++ src/pythonprop/voaP2PPlot.py
@@ -212,7 +212,7 @@ class VOAP2PPlot:
             else :
                 y_max = math.ceil(plot_max_freq / 5.0) * 5.0
             #resize the image
-            image_buffer = image_buffer[0:y_max-1,:]
+            image_buffer = image_buffer[0:int(y_max-1),:]
 
             y_ticks = [2, 5]
             for y_tick_value in np.arange(10, y_max+1, 5):
