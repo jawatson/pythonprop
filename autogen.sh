15649ae17fa60f22f833cc6ac35006c2d238d2ad
James Watson <jimwatson@mac.com>
2020-05-25 12:46:23 +0300

    Adds a check for yelp-tools

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,14 @@
 #! /bin/sh
 
+GNOMEDOC=`which yelp-build`
+if test -z $GNOMEDOC; then
+        echo "*** The tools to build the documentation are not found,"
+        echo "    please intall the yelp-tool package ***"
+        exit 1
+fi
+
 mkdir -p m4 \
-  && aclocal -I m4 \
-  && yelp-build \
-  && aclocal -I m4
+  && aclocal -I m4 
 
 aclocal \
   && automake --add-missing \
